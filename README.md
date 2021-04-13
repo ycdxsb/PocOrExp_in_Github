970db441d7561591db814ba5a07d80ca93ce25f4
ycdxsb <1452740432@qq.com>
2021-04-13 13:47:08 +0800

    add README and scripts

---
+++ README.md
@@ -0,0 +1,61 @@
+<h1 align="center">PocOrExp in Github</h1>
+
+<p align="center">
+  <img      src="https://img.shields.io/badge/python3-3.6-blue"/>
+  <img      src="https://img.shields.io/github/stars/ycdxsb/PocOrExp_in_Github"/>  
+  <img      src="https://img.shields.io/github/forks/ycdxsb/PocOrExp_in_Github"/> 
+  <img      src="https://img.shields.io/github/issues/ycdxsb/PocOrExp_in_Github"/> 
+</p>     
+
+> 聚合Github上已有的Poc或者Exp，CVE信息来自CVE官网
+>
+> 注意：只通过通用的CVE号聚合，因此对于MS17-010等Windows编号漏洞以及著名的有绰号的漏洞，还是自己检索一下比较好
+
+## Usage
+
+```
+usage: exp.py [-h]
+              [-y {1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,all}]
+
+CVE Details and Collect PocOrExp in Github
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -y {1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,all}, --year {1999,2000,2001,2002,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020,2021,all}
+                        get Poc or CVE of certain year or all years
+```
+
+- STEP1：安装依赖
+
+  ```
+  pip3 install tqdm aiohttp_requests
+  ```
+
+- STEP2：申请github api token写入项目目录下的TOKENS文件中，格式如下，可以使用多个token：
+
+  ```
+  token:your_token
+  ```
+
+- STEP3：处理cve信息
+
+  ```
+  处理某一年：python3 exp.py -y 2021
+  处理全部：python3 exp.py -y all
+  ```
+
+- 如果想要加快速度，可使用异步版脚本exp_async.py
+
+最终结果呈现在每个年份目录下的README.md，以及根目录下的PocOrExp.md中
+
+
+## ChangeLog
+- 20200411: 修改查询语句，从原来的模糊匹配转为严格匹配
+- 20200412: 修改了github markdown渲染问题，可使用exp_markdown_for_github.py
+- 20200412: 修复了github搜索时，例如搜索CVE-2020-3618，会搜索到CVE-2020-36184的结果的问题
+- 20200412: 优化前缀搜索问题的处理方案
+- 20200413：由于NVD官网CVE编号确实，改用CVE官网数据，发布异步版脚本
+
+
+## Reference
+- https://github.com/nomi-sec/PoC-in-GitHub
---
+++ exp.py
@@ -0,0 +1,246 @@
+import json
+import os
+import shutil
+import requests
+import argparse
+import datetime
+import time
+from random import sample
+from tqdm import tqdm
+
+DOWNLOAD_DIR = 'download'
+TOKEN_FILE = 'TOKENS'
+tokens = []
+
+def download_cve_xml(filename):
+    base_url = "https://cve.mitre.org/data/downloads/"
+    url = base_url + filename
+    xml_content = requests.get(url, stream=True)
+    with open(os.path.join(DOWNLOAD_DIR, filename), 'wb') as f:
+        for chunk in xml_content:
+            f.write(chunk)
+
+def download_cve_xml_all():
+    filenames = ["allitems-cvrf-year-%d.xml"%year for year in range(1999,datetime.datetime.now().year+1)]
+    for filename in filenames:
+        download_cve_xml(filename)
+   
+def parse_cve_xml(filename):
+    with open(os.path.join(DOWNLOAD_DIR, filename),'rb') as f:
+        content = f.read().split(b'\n')
+    cve_infos = []
+    cve_ids = []
+    cve_descriptions = []
+    for line in content:
+        line = line.strip()
+        if line.startswith(b"<CVE>"):
+            cve_ids.append(line[5:-6].decode(encoding='utf-8'))
+        if line.startswith(b'<Note Ordinal="1" Type="Description">'):
+            cve_descriptions.append(line[37:-7].decode('utf-8',"ignore"))
+    cve_infos = []
+    if(len(cve_ids)!=len(cve_descriptions)):
+        print("error")
+        return cve_infos
+    else:
+        for i in range(len(cve_ids)):
+            cve_infos.append({'CVE_ID': cve_ids[i], 'CVE_DESCRIPTION': cve_descriptions[i]})
+        return cve_infos
+    
+def generate_markdown_year(year):
+    year = str(year)
+    filenames = os.listdir(year)
+    filenames = [filename for filename in filenames if filename.startswith('CVE')]
+    cve_number = [[int(filename.split('.')[0].split('-')[-1]),filename] for filename in filenames]
+    cve_number.sort( key=lambda e:e[0], reverse=True)
+    string = []
+    for number in cve_number:
+        filename = os.path.join(year,number[1])
+        print(filename)
+        with open(filename,'r') as f:
+            cve_info = json.load(f)
+            if(cve_info['PocOrExp_NUM']==0):
+                continue
+            else:
+                string.append("## %s" % cve_info['CVE_ID'])
+                string.append("> %s\n" % cve_info['CVE_DESCRIPTION'])
+                string.append("\n")
+                for PocOrExp in cve_info['PocOrExp']:
+                    URL = PocOrExp['URL']
+                    AUTHOR = URL.split('/')[-2]
+                    PROJECT_NAME = URL.split('/')[-1]
+                    link = "- [%s](%s) : " % (URL,URL)
+                    stars = "![starts](https://img.shields.io/github/stars/%s/%s.svg)" %(AUTHOR,PROJECT_NAME)
+                    forks = "![forks](https://img.shields.io/github/forks/%s/%s.svg)" %(AUTHOR,PROJECT_NAME)
+                    string.append(" ".join([link,stars,forks])+'\n')
+    with open(os.path.join(year,"README.md"),'w') as f:
+        tmp = "\n".join(string)
+        tmp = tmp.replace("<","")
+        tmp = tmp.replace(">","")
+        f.write(tmp)
+    return string
+
+def generate_markdown():
+    PocOrExps = []
+    for year in list(range(1999, datetime.datetime.now().year+1))[::-1]:
+        string = generate_markdown_year(year)
+        PocOrExps.append('## %d' % year)
+        PocOrExps = PocOrExps + string
+        PocOrExps.append('\n')
+    with open('PocOrExp.md','w') as f:
+        tmp = '\n'.join(PocOrExps)
+        tmp = tmp.replace("<","")
+        tmp = tmp.replace(">","")
+        f.write(tmp)
+
+def get_PocOrExp_in_github(CVE_ID,Other_ID = None):
+    if(Other_ID == None):
+        api = 'https://api.github.com/search/repositories?q="%s"&sort=stars' % CVE_ID
+    else:
+        api = 'https://api.github.com/search/repositories?q="%s" NOT "%s"&sort=stars' % (CVE_ID,Other_ID)
+    if(len(tokens)>=4):
+        windows = 0
+    else:
+        windows = 0.8/len(tokens)
+    while(True):
+        time.sleep(windows)
+        TOKEN = sample(tokens,1)[0]
+        headers = {"Authorization": "token "+TOKEN}
+        req = requests.get(api, headers=headers).text
+        req = json.loads(req)
+        print(CVE_ID,Other_ID,req)
+        if('items' in req):
+            items = req['items']
+            break
+        else:
+            windows = windows + 0.1
+    PocOrExps = []
+    for item in items:
+        URL = item['html_url']
+        STARS_NUM = item['stargazers_count']
+        FORKS_NUM = item['forks_count']
+        DESCRIPTION = item['description']
+        UPDATE_TIME = item['updated_at']
+        PocOrExps.append({
+            'URL': URL,
+            'STARS_NUM': STARS_NUM,
+            'FORKS_NUM': FORKS_NUM,
+            'DESCRIPTION': DESCRIPTION,
+            'UPDATE_TIME': UPDATE_TIME
+        })
+    return PocOrExps
+
+
+def parse_arg():
+    parser = argparse.ArgumentParser(
+        description='CVE Details and Collect PocOrExp in Github')
+    parser.add_argument('-y', '--year',required=False,default=None, choices=list(map(str,range(1999,datetime.datetime.now().year+1)))+['all'],
+                        help="get Poc or CVE of certain year or all years")
+    args = parser.parse_args()
+    return args
+
+def is_prefix(cve_ids,CVE_ID):
+    for cve_id in cve_ids:
+        if cve_id!= CVE_ID and cve_id.startswith(CVE_ID):
+            return True
+    return False
+
+def get_all_startswith_CVE_ID(cve_ids,CVE_ID):
+    result = []
+    for cve_id in cve_ids:
+        if cve_id!= CVE_ID and cve_id.startswith(CVE_ID):
+            result.append(cve_id)
+    return result
+
+def process_cve(cve_infos,init = True):
+    cve_ids = []
+    for item in cve_infos:
+        cve_ids.append(item['CVE_ID'])
+    for item in tqdm(cve_infos):
+        CVE_ID = item['CVE_ID']
+        year = item['CVE_ID'].split("-")[1]
+        dump_file = os.path.join(year, CVE_ID + '.json')
+        PocOrExps = []
+        if(not is_prefix(cve_ids,CVE_ID)):
+            PocOrExps = get_PocOrExp_in_github(CVE_ID)
+        else:
+            PocOrExps = get_PocOrExp_in_github(CVE_ID)
+            if(len(PocOrExps)!=0):
+                other_ids = get_all_startswith_CVE_ID(cve_ids,CVE_ID)
+                PocOrExps = get_PocOrExp_in_github(CVE_ID)
+                for other_id in other_ids:
+                    tmp = get_PocOrExp_in_github(CVE_ID,other_id)
+                    urls_CVE_ID = []
+                    urls_Other_ID = []
+                    for PocOrExp in PocOrExps:
+                        urls_CVE_ID.append(PocOrExp['URL'])
+                    for PocOrExp in tmp:
+                        urls_Other_ID.append(PocOrExp['URL'])
+                    urls_CVE_ID = list(set(urls_CVE_ID) & set(urls_Other_ID))
+                    tmp = PocOrExps
+                    PocOrExps = []
+                    for PocOrExp in tmp:
+                        if(PocOrExp['URL'] in urls_CVE_ID):
+                            PocOrExps.append(PocOrExp)
+        cve_info = {}
+        cve_info['CVE_ID'] = item['CVE_ID']
+        cve_info['CVE_DESCRIPTION'] = item['CVE_DESCRIPTION']
+        cve_info['PocOrExp_NUM'] = len(PocOrExps)
+        cve_info['PocOrExp'] = PocOrExps
+        with open(dump_file, 'w') as f:
+            json.dump(cve_info, f)
+
+
+def process_cve_year(year,init = True):
+    filename = "allitems-cvrf-year-%d.xml"%year
+    download_cve_xml(filename)
+    cve_infos = parse_cve_xml(filename)
+    tmp = []
+    if(init):
+        for item in cve_infos:
+            CVE_ID = item['CVE_ID']
+            year = item['CVE_ID'].split("-")[1]
+            dump_file = os.path.join(year, CVE_ID + '.json')
+            if(init and os.path.exists(dump_file)):
+                continue
+            else:
+                tmp.append(item)
+    cve_infos = tmp
+    process_cve(cve_infos,init)
+    generate_markdown()
+    
+def process_cve_all(init = True):
+    for year in list(range(1999,datetime.datetime.now().year+1))[::-1]:
+        process_cve_year(year,init)
+
+def init():
+    if(os.path.exists(DOWNLOAD_DIR)):
+        shutil.rmtree(DOWNLOAD_DIR)
+    os.mkdir(DOWNLOAD_DIR)
+    for year in range(1999, datetime.datetime.now().year+1):
+        if(not os.path.exists(str(year))):
+            os.mkdir(str(year))
+    if(not os.path.exists(TOKEN_FILE)):
+        print("please checkout your token files")
+        exit(-1)
+    global tokens
+    with open(TOKEN_FILE) as f:
+        content = f.readlines()
+    for line in content:
+        line = line.strip()
+        if line.startswith("token:"):
+            tokens.append(line.split(":")[-1])
+    print(tokens)
+    if(len(tokens)==0):
+        print("please checkout your token files")
+    
+def main():
+    args = parse_arg()
+    init()
+    print(args)
+    if(args.year == "all"):
+        process_cve_all()
+    elif(args.year):
+        process_cve_year(int(args.year))
+    
+if __name__=="__main__":
+    main()
